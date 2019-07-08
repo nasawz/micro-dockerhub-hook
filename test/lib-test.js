199ef70e94f50ea83c79cfc2ee6bdcb4fe5add8e
nasa.wang <nasa.wang@nicesn.com>
2019-07-08 13:18:31 +0800

    aliyun

--- test/lib-test.js
+++ test/lib-test.js
@@ -1,84 +1,83 @@
-process.env.DEBUG = 'false'
-const test = require('ava')
-const validateReq = require('../lib/validate-req')
-const config = require('../config')
-const hooks = require('../scripts')
+process.env.DEBUG = "false";
+const test = require("ava");
+const validateReq = require("../lib/validate-req");
+const config = require("../config");
+const hooks = require("../scripts");
 
-test('no token given', t => {
+test("no token given", t => {
   try {
-    validateReq({})
+    validateReq({});
   } catch (e) {
-    t.is(e.message, 'No token given')
+    t.is(e.message, "No token given");
   }
-})
+});
 
-test('invalid token', t => {
+test("invalid token", t => {
   try {
-    const pathname = '/invalid'
-    validateReq({ pathname })
+    const pathname = "/invalid";
+    validateReq({ pathname });
   } catch (e) {
-    t.is(e.message, 'Invalid token')
+    t.is(e.message, "Invalid token");
   }
-})
+});
 
-test('missing payload', t => {
+test("missing payload", t => {
   try {
-    const pathname = `/${config.token}`
-    validateReq({ pathname })
+    const pathname = `/${config.token}`;
+    validateReq({ pathname });
   } catch (e) {
-    t.is(e.message, 'Missing payload')
+    t.is(e.message, "Missing payload");
   }
-})
+});
 
-test('Missing payload.push_data', t => {
+test("Missing payload.push_data", t => {
   try {
-    const pathname = `/${config.token}`
-    const payload = {}
-    validateReq({ pathname, payload })
+    const pathname = `/${config.token}`;
+    const payload = {};
+    validateReq({ pathname, payload });
   } catch (e) {
-    t.is(e.message, 'Missing payload.push_data')
+    t.is(e.message, "Missing payload.push_data");
   }
-})
+});
 
-test('missing payload.repository', t => {
+test("missing payload.repository", t => {
   try {
-    const pathname = `/${config.token}`
+    const pathname = `/${config.token}`;
     const payload = {
       push_data: {}
-    }
-    validateReq({ pathname, payload })
+    };
+    validateReq({ pathname, payload });
   } catch (e) {
-    t.is(e.message, 'Missing payload.repository')
+    t.is(e.message, "Missing payload.repository");
   }
-})
+});
 
-test('Missing payload.repository.repo_name', t => {
+test("Missing payload.repository.repo_full_name", t => {
   try {
-    const pathname = `/${config.token}`
+    const pathname = `/${config.token}`;
     const payload = {
       push_data: {},
-      repository: {
-      }
-    }
-    validateReq({ pathname, payload })
+      repository: {}
+    };
+    validateReq({ pathname, payload });
   } catch (e) {
-    t.is(e.message, 'Missing payload.repository.repo_name')
+    t.is(e.message, "Missing payload.repository.repo_full_name");
   }
-})
+});
 
-test('Repo does not exist', t => {
+test("Repo does not exist", t => {
   try {
-    const pathname = `/${config.token}`
+    const pathname = `/${config.token}`;
     const payload = {
       push_data: {
-        tag: ''
+        tag: ""
       },
       repository: {
-        repo_name: 'maccyber/notexist'
+        repo_name: "maccyber/notexist"
       }
-    }
-    validateReq({ pathname, payload, hooks })
+    };
+    validateReq({ pathname, payload, hooks });
   } catch (e) {
-    t.is(e.message, 'maccyber/notexist does not exist in scripts/index.js')
+    t.is(e.message, "maccyber/notexist does not exist in scripts/index.js");
   }
-})
+});
